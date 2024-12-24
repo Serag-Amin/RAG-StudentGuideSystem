b73c93c74daf026fa621ccd31b0ed4603dce9260
Serag-Amin <115029341+Serag-Amin@users.noreply.github.com>
2024-12-24 18:34:59 +0000

    Revert back until futher updates

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import streamlit as st
 
 load_dotenv()
 
@@ -10,14 +9,4 @@ class Config:
     CHUNK_OVERLAP = 75
     DB_NAME = "vectdb"
     N_RESULTS = 5
-
-    # Get API key from environment variables or Streamlit secrets
-    def get_google_api_key():
-        if "GOOGLE_API_KEY" in os.environ:  # locally
-            return os.getenv("GOOGLE_API_KEY")
-        elif "GOOGLE_API_KEY" in st.secrets:  # Streamlit's secrets manager
-            return st.secrets["GOOGLE_API_KEY"]
-        else:
-            raise ValueError("Google API key not found. Please set it in your environment or Streamlit secrets.")
-
-google_api_key = get_google_api_key()
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "default-api-key")  
